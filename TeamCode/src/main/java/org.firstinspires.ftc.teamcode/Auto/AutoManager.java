42e00b79513a872ccfe19e15c4bb90243ac0f595
TM-Codes <michiejt@gmail.com>
2023-12-02 11:37:41 -0700

    Start creating Automanager and an auto for Red alliance Back

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/AutoManager.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Core.DrivetrainCore;
+import org.firstinspires.ftc.teamcode.Core.PIDController;
+
+public abstract class AutoManager extends LinearOpMode {
+    public DrivetrainCore drivetrain;
+
+
+}
---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/AutoRedBack.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name = "AutoRedBack")
+public abstract class AutoRedBack extends AutoManager {
+
+}
